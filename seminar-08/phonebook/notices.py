c1e4a1e0720343edfa03bf3428c9608eff705556
nailharr <nailharr@gmail.com>
2023-07-22 00:26:36 +0300

    phone book - complete add, print and find contacts + 12 contacts fill

---
+++ seminar-08/phonebook/notices.py
@@ -0,0 +1,29 @@
+line = '--------------------------------------\n'
+
+
+def msg(x):
+    invalid_choice = 'Invalid number of choice. Try again.'
+    done_new_contact = 'Контакт успешно добавлен'
+    done_change_contact = 'Контакт успешно изменен'
+    done_delete_contact = 'Контакт удален'
+    end_list = 'Конец списка'
+    end_prog = 'Программа завершена'
+    end_find = 'Поиск завершен'
+    sort_done = 'Контакты отсортированы'
+
+    if x == 0:
+        return end_prog
+    elif x == 1:
+        return done_new_contact
+    elif x == 2:
+        return end_list
+    elif x == 3:
+        return end_find
+    elif x == 4:
+        return sort_done
+    elif x == 5:
+        return done_change_contact
+    elif x == 6:
+        return done_delete_contact
+    else:
+        return invalid_choice
---
+++ seminar-08/phonebook/user_func.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+def input_number():
+    ask = int(input("1 - Создать новый контакт\n"
+                    "2 - Показать список контактов\n"
+                    "3 - Найти контакт в справочнике\n"
+                    "4 - Отсортировать контакты\n"
+                    "5 - Изменить контакт\n"
+                    "6 - Удалить контакт\n"
+                    "… - \n"
+                    "0 - Exit program\n"
+                    "-------------------\n"
+                    "Выберите действие: "))
+    return ask
+
+
+def input_contact():  # TODO: refactoring need
+    contact_list = []
+    contact_id = contact_list.append(int(input("Порядковый номер: ")))  # TODO: add autoincrement
+    lastname = contact_list.append(input("Фамилия: "))
+    name = contact_list.append(input("Имя: "))
+    surname = contact_list.append(input("Отчество: "))
+    phone_number = contact_list.append(input("Номер телефона: "))
+    date_of_birth = contact_list.append(datetime.strptime(input("Дата рождения — DD.MM.YYY: "), '%d.%m.%Y'))
+    return contact_list
+
+
+def input_feature():
+    request_feature = input("Введите искомое значение: \n")
+    return request_feature
+
